e60290074d730f0d021d5dc3f98e3693fdc13e9b
ericvanderford <ericvanderford123@gmail.com>
2021-11-22 15:37:18 -0500

    class 11/22

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+
 import csv
+
+with open('data folder/coffees.csv') as f:
+    csv_reader = csv.reader(f)
+
+
+    next(csv_reader)
+
+    # show the data
+    for line in csv_reader:
+        print(line)
